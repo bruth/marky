bda3af9f9c783d1b40104183a127f433c520e8a8
Byron Ruth <b@devel.io>
2012-09-03 23:26:47 -0400

    Separate out renderer view Add support for `text/plain` and `application/json` Remove unnecessary CSRF protection (for now) Add cross-origin support, although this needs to be more restricted

--- marky/urls.py
+++ marky/urls.py
@@ -3,4 +3,5 @@ from django.conf.urls import patterns, url
 
 urlpatterns = patterns('marky.views',
     url(r'^$', 'preview', name='marky-preview'),
+    url(r'^api/$', 'api', name='marky-api'),
 )
--- marky/views.py
+++ marky/views.py
@@ -1,6 +1,8 @@
+import json
 from django.http import HttpResponse
 from django.shortcuts import render
-from django.views.decorators.csrf import csrf_protect
+from django.views.decorators.http import require_safe, require_POST
+from django.views.decorators.csrf import csrf_exempt
 from markdown import Markdown
 
 markdown = Markdown(output_format='html5', extensions=[
@@ -19,11 +21,29 @@ markdown = Markdown(output_format='html5', extensions=[
     'codehilite(css_class=highlight)',
 ])
 
-@csrf_protect
+@require_safe
 def preview(request):
-    if request.method == 'POST':
-        markup = request.POST.get('markup', '')
-        markdown.reset()
-        return HttpResponse(markdown.convert(markup))
     return render(request, 'marky/preview.html')
 
+@csrf_exempt
+@require_POST
+def api(request):
+    response = HttpResponse()
+    # TODO change this to check for registered hosts either as a setting
+    # or database records
+    response['Access-Control-Allow-Origin'] = '*'
+
+    # Parse request body accordingly.. JSON, plain text and form data are
+    # supported.
+    content_type = request.META['CONTENT_TYPE']
+
+    if 'application/json' in content_type:
+        markup = json.loads(request.body).get('markup', '')
+    elif 'text/plain' in content_type:
+        markup = request.body
+    else:
+        markup = request.POST.get('markup', '')
+
+    markdown.reset()
+    response.content = markdown.convert(markup)
+    return response
